a2c01e543a328e5aa638a14c557af84d86783403
weihu <hu.wei@vpclub.cn>
2020-06-13 20:08:14 +0800

    登录接口联调

--- app/src/main/java/com/ttit/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/ttit/myapp/activity/LoginActivity.java
@@ -3,13 +3,29 @@ package com.ttit.myapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ttit.myapp.R;
+import com.ttit.myapp.util.AppConfig;
 import com.ttit.myapp.util.StringUtils;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends BaseActivity {
 
@@ -43,5 +59,43 @@ public class LoginActivity extends BaseActivity {
             showToast("请输入密码");
             return;
         }
+
+        //第一步创建OKHttpClient
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        Map m = new HashMap();
+        m.put("mobile", account);
+        m.put("password", pwd);
+        JSONObject jsonObject = new JSONObject(m);
+        String jsonStr = jsonObject.toString();
+        RequestBody requestBodyJson =
+                RequestBody.create(MediaType.parse("application/json;charset=utf-8")
+                        , jsonStr);
+        //第三步创建Rquest
+        Request request = new Request.Builder()
+                .url(AppConfig.BASE_URl + "/app/login")
+                .addHeader("contentType", "application/json;charset=UTF-8")
+                .post(requestBodyJson)
+                .build();
+        //第四步创建call回调对象
+        final Call call = client.newCall(request);
+        //第五步发起请求
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("onFailure", e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showToast(result);
+                    }
+                });
+            }
+        });
     }
 }
